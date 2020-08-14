50d744e425f937e8877eb7ef2eb6883fc6bb05d1
Remir <remirboy@yandex.ru>
2020-08-14 11:29:00 +0300

    BurgerMenu and FlexBox

---
+++ my-app/src/components/app/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import { Footer} from '../footer'
+import { Main} from '../main'
+import { Header} from '../header'
+
+const App = () =>(
+    <Fragment>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+    </Fragment>
+)
+
+export { App}
---
+++ my-app/src/components/main/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Main = () =>(
+    <div className='main'>Main</div>
+)
+
+export { Main}
